cf3d69aec5289b43d5c9080f2760c44c24275130
Piotr Fidurski <crunor55@hotmail.com>
2021-12-27 13:24:36 +0100

    chore: update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,15 @@ module.exports = {
   content: ['./app/**/*.{ts,tsx}'],
   theme: {
     extend: {
+      margin: {
+        tiny: '0.15rem',
+      },
+      colors: {
+        error: '#B40023',
+        white: {
+          10: 'rgba(255 255 255 / 10%)',
+        },
+      },
       fontFamily: {
         sans: [
           'Montserrat',
@@ -13,6 +22,9 @@ module.exports = {
       },
       minHeight: {
         900: '900px',
+      },
+      maxWidth: {
+        76: '76',
       },
     },
   },
