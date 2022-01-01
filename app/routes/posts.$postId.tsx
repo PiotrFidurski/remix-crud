40c31e84bd5f224694f2c11f52f3d8cca789ce40
Piotr Fidurski <crunor55@hotmail.com>
2022-01-01 12:47:46 +0100

    chore: fetch post from db

--- app/routes/posts.$postId.tsx
+++ app/routes/posts.$postId.tsx
@@ -1,12 +1,18 @@
 import { LoaderFunction, useLoaderData } from 'remix';
 import { getUser } from '~/features/auth/utils/getUser';
 import { Post } from '~/features/posts/components/Post';
+import { db } from '~/utils/db.server';
 
 export const loader: LoaderFunction = async ({
   request,
+  params,
 }) => {
+  const { postId } = params;
   const user = await getUser(request);
-  return { user };
+  const post = await db.post.findFirst({
+    where: { id: postId },
+  });
+  return { user, post };
 };
 
 export default function PostRoute() {
@@ -14,16 +20,7 @@ export default function PostRoute() {
 
   return (
     <div className="flex flex-col gap-4 w-full bg-black-default rounded-md px-4 py-8">
-      <Post
-        user={data.user}
-        post={{
-          id: 1,
-          createdAt: new Date(),
-          title: 'christmas dog',
-          content:
-            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur unde reiciendis culpa facere eaque sint doloremque id sequi ipsum aperiam recusandae magnam quaerat, minus asperiores reprehenderit dolorum itaque. Excepturi, corporis.',
-        }}
-      />
+      <Post user={data.user} post={data.post} />
     </div>
   );
 }
