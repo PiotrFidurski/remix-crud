5c09f90717e62a1201086aca5b975de496a872d3
Piotr Fidurski <crunor55@hotmail.com>
2022-01-01 12:46:25 +0100

    feat: add create post functionality
    Wire up action to create post Form and create post create schema

--- app/routes/posts.new.tsx
+++ app/routes/posts.new.tsx
@@ -1,14 +1,20 @@
 import {
   ActionFunction,
   Form,
+  json,
   LoaderFunction,
+  redirect,
+  useActionData,
 } from 'remix';
+import { ZodError } from 'zod';
 import { Button } from '~/components/Elements';
 import {
   InputField,
   TextareaField,
 } from '~/components/Form';
 import { requireUserId } from '~/features/auth/utils/getUser';
+import { schema } from '~/features/posts/utils/createPostSchema';
+import { db } from '~/utils/db.server';
 
 export const loader: LoaderFunction = async ({
   request,
@@ -17,17 +23,59 @@ export const loader: LoaderFunction = async ({
   return null;
 };
 
+type ActionData = {
+  formError?: string;
+  fieldErrors?: {
+    title: string | undefined;
+    content: string | undefined;
+  };
+};
+
+const badRequest = (data: ActionData) =>
+  json(data, { status: 400 });
+
 export const action: ActionFunction = async ({
   request,
 }) => {
-  await requireUserId(request);
-  return null;
+  const form = await request.formData();
+  const title = form.get('title') as string;
+  const content = form.get('content') as string;
+  try {
+    const userId = await requireUserId(request);
+
+    schema.parse({ title, content });
+
+    const post = await db.post.create({
+      data: {
+        title,
+        content,
+        authorId: userId,
+      },
+    });
+
+    return redirect(`/posts/${post.id}`);
+  } catch (error) {
+    const errors = (error as ZodError).flatten();
+
+    return badRequest({
+      fieldErrors: {
+        title: errors.fieldErrors.title?.[0],
+        content: errors.fieldErrors.content?.[0],
+      },
+    });
+  }
 };
 
 export default function NewPostRoute() {
+  const actionData = useActionData<ActionData>();
   return (
     <div className="bg-black-default p-2 text-gray-300 min-h-screen flex justify-center">
       <Form
+        aria-describedby={
+          actionData?.formError
+            ? 'form-error-message'
+            : undefined
+        }
         method="post"
         className="flex px-2 flex-col max-w-2xl w-full m-auto"
       >
@@ -35,21 +83,39 @@ export default function NewPostRoute() {
           Create new post
         </h1>
         <InputField
+          errorMessage={actionData?.fieldErrors?.title}
           htmlFor="title"
           type="text"
           name="title"
           required
           minLength={5}
           maxLength={30}
+          aria-invalid={Boolean(
+            actionData?.fieldErrors?.title
+          )}
+          aria-describedby={
+            actionData?.fieldErrors?.title
+              ? 'username-error'
+              : undefined
+          }
         >
           Title
         </InputField>
         <TextareaField
+          errorMessage={actionData?.fieldErrors?.content}
           name="content"
           htmlFor="content"
           required
           minLength={50}
           maxLength={300}
+          aria-invalid={Boolean(
+            actionData?.fieldErrors?.content
+          )}
+          aria-describedby={
+            actionData?.fieldErrors?.content
+              ? 'username-error'
+              : undefined
+          }
         >
           Content
         </TextareaField>
