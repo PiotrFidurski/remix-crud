af251a6a237e6402633ed6ea785a8d12b444f867
Piotr Fidurski <crunor55@hotmail.com>
2022-01-03 13:01:02 +0100

    refactor: rename canDelete prop
    renamed canDelete to canModify prop to better describe it, added Remix
    Link to go to edit page

--- app/features/posts/components/PostDropDown.tsx
+++ app/features/posts/components/PostDropDown.tsx
@@ -1,4 +1,5 @@
 import * as DropdownMenu from '@radix-ui/react-dropdown-menu';
+import { Link } from 'remix';
 import {
   Button,
   DropdownItem,
@@ -10,11 +11,13 @@ import {
 } from '~/components/Icons';
 
 type PostDropdownProps = {
-  canDelete: boolean;
+  canModify: boolean;
+  postId: string;
 };
 
 export function PostDropdown({
-  canDelete,
+  canModify,
+  postId,
 }: PostDropdownProps) {
   return (
     <DropdownMenu.Root>
@@ -43,18 +46,21 @@ export function PostDropdown({
             />
           </div>
         </DropdownItem>
-        {canDelete ? (
+        {canModify ? (
           <DropdownItem>
-            <div className="px-2 py-1 min-w-200 w-full flex justify-between items-center text-sm">
+            <Link
+              to={`posts/${postId}/edit`}
+              className="px-2 py-1 min-w-200 w-full flex justify-between items-center text-sm"
+            >
               <span>Edit</span>
               <ArrowRightIcon
                 aria-hidden="true"
                 className="w-5 h-5"
               />
-            </div>
+            </Link>
           </DropdownItem>
         ) : null}
-        {canDelete ? (
+        {canModify ? (
           <DropdownItem className="hover:bg-error focus:bg-error">
             <div className="px-2 py-1 min-w-200 w-full flex justify-between items-center text-sm">
               <span>Delete</span>
