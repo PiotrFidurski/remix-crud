07d0bb1121d2f2d3ef8a7fcd5ba6c7868ef38cdd
Piotr Fidurski <crunor55@hotmail.com>
2022-01-03 11:52:38 +0100

    refactor: simplify display response component
    Removed OkResponse and ErrorResponse components, if we want something
    more later we can add switch on caught.status later

--- app/components/Responses/DisplayResponse.tsx
+++ app/components/Responses/DisplayResponse.tsx
@@ -1,35 +1,21 @@
 import { useCatch } from 'remix';
-import { ErrorResponse } from './ErrorResponse';
-import { OkResponse } from './OkResponse';
+import { SadEmojiIcon } from '../Icons';
 
-type DisplayErrorProps =
-  React.HTMLAttributes<HTMLDivElement> & {
-    icon: React.ReactNode;
-    children?: React.ReactNode;
-  };
-
-export function DisplayResponse({
-  icon,
-  children,
-}: DisplayErrorProps) {
+export function DisplayResponse() {
   const caught = useCatch();
 
-  switch (caught.status) {
-    case 200: {
-      return (
-        <OkResponse icon={icon}>{children}</OkResponse>
-      );
-    }
-    case 404: {
-      return <ErrorResponse icon={icon} />;
-    }
-
-    case 500: {
-      return <ErrorResponse icon={icon} />;
-    }
-
-    default: {
-      return <ErrorResponse icon={icon} />;
-    }
-  }
+  return (
+    <div className="flex flex-col justify-center min-h-screen gap-4 w-full text-red-400 bg-black-default rounded-md px-4 py-8">
+      <div className="flex flex-col gap-4 border-2 px-6 py-6 border-red-400 border-dashed justify-center items-center text-center max-w-lg m-auto">
+        <div className="flex flex-col items-center">
+          <SadEmojiIcon className="w-12 h-12" />
+          <h1 className="font-bold">{caught.statusText}</h1>
+          <p>{caught.status}</p>
+        </div>
+        <h2 className="text-3xl text-gray-300">
+          {caught.data}
+        </h2>
+      </div>
+    </div>
+  );
 }
