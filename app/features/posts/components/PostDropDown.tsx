dd44d5092bda7ea0696e3838b539453982cfea8d
Piotr Fidurski <crunor55@hotmail.com>
2022-01-03 13:06:53 +0100

    refactor: update PostDropdown
    Updated Dropdown item for edit so we can press enter to go to edit page

--- app/features/posts/components/PostDropDown.tsx
+++ app/features/posts/components/PostDropDown.tsx
@@ -47,18 +47,18 @@ export function PostDropdown({
           </div>
         </DropdownItem>
         {canModify ? (
-          <DropdownItem>
-            <Link
-              to={`posts/${postId}/edit`}
-              className="px-2 py-1 min-w-200 w-full flex justify-between items-center text-sm"
-            >
+          <Link
+            to={`posts/${postId}/edit`}
+            className="min-w-200 w-full items-center text-sm"
+          >
+            <DropdownItem className="flex justify-between px-2 py-1">
               <span>Edit</span>
               <ArrowRightIcon
                 aria-hidden="true"
                 className="w-5 h-5"
               />
-            </Link>
-          </DropdownItem>
+            </DropdownItem>
+          </Link>
         ) : null}
         {canModify ? (
           <DropdownItem className="hover:bg-error focus:bg-error">
