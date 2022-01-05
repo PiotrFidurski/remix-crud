74cfb27542a7584ece8ca108546c8c8c51934956
Piotr Fidurski <crunor55@hotmail.com>
2022-01-05 12:05:04 +0100

    refactor: extract Document component
    Added Document component that renders our html, scripts, metas etc
    and accepts children as prop where we can either render happy path or
    ErrorBoundary stuff

--- app/root.tsx
+++ app/root.tsx
@@ -64,16 +64,11 @@ export const loader: LoaderFunction = async ({
   return data;
 };
 
-export default function App() {
-  const { user } = useLoaderData<LoaderData>();
-
-  const transition = useTransition();
-
-  React.useEffect(() => {
-    if (transition.state === 'idle') nProgress.done();
-    else nProgress.start();
-  }, [transition.state]);
-
+function Document({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
       <head>
@@ -86,18 +81,7 @@ export default function App() {
         <Links />
       </head>
       <body className="bg-gray-background">
-        <AuthProvider user={user!}>
-          <main className="max-w-7xl m-auto w-100 text-white">
-            <div className="grid grid-cols-4 gap-2 sm:p-10 p-0">
-              <div className="lg:col-span-1 col-span-4 h-20">
-                <Sidebar />
-              </div>
-              <div className="col-span-4 lg:col-span-3">
-                <Outlet />
-              </div>
-            </div>
-          </main>
-        </AuthProvider>
+        {children}
         <ScrollRestoration />
         <Scripts />
         {process.env.NODE_ENV === 'development' && (
@@ -108,39 +92,59 @@ export default function App() {
   );
 }
 
-export function ErrorBoundary({ error }: { error: Error }) {
+export default function App() {
+  const { user } = useLoaderData<LoaderData>();
+
+  const transition = useTransition();
+
+  React.useEffect(() => {
+    if (transition.state === 'idle') nProgress.done();
+    else nProgress.start();
+  }, [transition.state]);
+
   return (
-    <html lang="en">
-      <head>
-        <title>Oh noes!</title>
-        <Meta />
-        <Links />
-      </head>
-      <body>
-        <div className="bg-black-default">
-          <div className="flex max-w-lg text-gray-300 flex-col justify-center min-h-screen m-auto gap-4 w-full rounded-md px-4 py-8">
-            <div className="flex flex-col items-center text-red-400">
-              <SadEmojiIcon className="w-24 h-24" />
-              <h1 className="py-2 text-3xl">
-                something went very wrong
-              </h1>
+    <Document>
+      <AuthProvider user={user!}>
+        <main className="max-w-7xl m-auto w-100 text-white">
+          <div className="grid grid-cols-4 gap-2 sm:p-10 p-0">
+            <div className="lg:col-span-1 col-span-4 h-20">
+              <Sidebar />
             </div>
-            <div>
-              <details>
-                <summary>
-                  <span className="text-gray-300">
-                    Error Details
-                  </span>
-                </summary>
-                <code className="text-red-400">
-                  {JSON.stringify(error.stack, null, 2)}
-                </code>
-              </details>
+            <div className="col-span-4 lg:col-span-3">
+              <Outlet />
             </div>
           </div>
+        </main>
+      </AuthProvider>
+    </Document>
+  );
+}
+
+export function ErrorBoundary({ error }: { error: Error }) {
+  return (
+    <Document>
+      <div className="bg-black-default">
+        <div className="flex max-w-lg text-gray-300 flex-col justify-center min-h-screen m-auto gap-4 w-full rounded-md px-4 py-8">
+          <div className="flex flex-col items-center text-red-400">
+            <SadEmojiIcon className="w-24 h-24" />
+            <h1 className="py-2 text-3xl">
+              something went very wrong
+            </h1>
+          </div>
+          <div>
+            <details>
+              <summary>
+                <span className="text-gray-300">
+                  Error Details
+                </span>
+              </summary>
+              <code className="text-red-400">
+                {JSON.stringify(error.stack, null, 2)}
+              </code>
+            </details>
+          </div>
         </div>
-        <Scripts />
-      </body>
-    </html>
+      </div>
+    </Document>
   );
 }
